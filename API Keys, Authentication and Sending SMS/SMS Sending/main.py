c6b2cc7961d9fea3906e02848d656759567dcd65
Tejashree-Tambe <68728212+Tejashree-Tambe@users.noreply.github.com>
2022-08-03 16:25:17 +0530

    erased keys

--- API Keys, Authentication and Sending SMS/SMS Sending/main.py
+++ API Keys, Authentication and Sending SMS/SMS Sending/main.py
@@ -2,10 +2,8 @@ import os
 from twilio.rest import Client
 
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['AC1abdc7ac82c6feba7a86c37f42d6f192']
-auth_token = os.environ['71a1122b85cc63c0bc54f63050323cd2']
+account_sid = os.environ['acc_sid']
+auth_token = os.environ['auth_token']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
