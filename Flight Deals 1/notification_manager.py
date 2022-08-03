d02a39dde34ffcf84041e05b3d922fdde58f2fae
Tejashree-Tambe <68728212+Tejashree-Tambe@users.noreply.github.com>
2022-08-03 16:42:00 +0530

    erased keys

--- Flight Deals 1/notification_manager.py
+++ Flight Deals 1/notification_manager.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
-account_sid = "AC1abdc7ac82c6feba7a86c37f42d6f192"
-auth_token = "71a1122b85cc63c0bc54f63050323cd2"
-FROM_NO = "+15207292541"
-TO_NO = "+919930049115"
+account_sid = "acc_sid"
+auth_token = "token"
+FROM_NO = "+from"
+TO_NO = "+to"
 
 
 class NotificationManager:
